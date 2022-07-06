f58180307de10c7a2125623541bbabc775e52684
Mario Antunes <mariolpantunes@gmail.com>
2022-07-06 15:34:36 +0100

    Improved the UI interface

--- static/script.js
+++ static/script.js
@@ -8,6 +8,8 @@ const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
+let previousBotCell = null;
+
 statusDisplay.innerHTML = currentPlayerTurn();
 
 const winningConditions = [
@@ -24,6 +26,7 @@ const winningConditions = [
 function handleCellPlayed(clickedCell, clickedCellIndex) {
     gameState[clickedCellIndex] = currentPlayer;
     clickedCell.innerHTML = currentPlayer;
+    clickedCell.style.color = '';
 }
 
 function handlePlayerChange() {
@@ -50,17 +53,18 @@ function handleResultValidation() {
     if (roundWon) {
         statusDisplay.innerHTML = winningMessage();
         gameActive = false; 
-        return;
+        return true;
     }
 
     let roundDraw = !gameState.includes("");
     if (roundDraw) {
         statusDisplay.innerHTML = drawMessage();
         gameActive = false;
-        return;
+        return true;
     }
 
     handlePlayerChange();
+    return false;
 }
 
 function httpGet(theUrl) {
@@ -70,12 +74,12 @@ function httpGet(theUrl) {
     return xmlHttp.responseText;
 }
 
-function handleBotPlay(index) {
-    row =  Math.trunc(index / 3);
-    col = index % 3;
+function requestBotPlay(index) {
+    //let row =  Math.trunc(index / 3),col = index % 3;
 
     // code the board into a an array
-    board = []
+    let board = []
+    let boardFull = true;
     for(let i = 0; i < 9; i++) {
         if(gameState[i] == "X") {
             board.push(1);
@@ -83,30 +87,42 @@ function handleBotPlay(index) {
             board.push(-1);
         } else {
             board.push(0);
+            boardFull = false;
         }
     }
 
-    console.log('Board = '+board);
-    url_parameters=''
-    for (const b of board) {
-        url_parameters += 'board='+b+'&'
-    }
-    url_parameters += 'player=-1'
-    
-    console.log('url parameters = '+url_parameters)
+    if(boardFull) {
+        return -1
+    } else {
+        //console.log('Board = '+board);
+        let url_parameters=''
+        for (const b of board) {
+            url_parameters += 'board='+b+'&'
+        }
+        url_parameters += 'player=-1'
+        
+        //console.log('url parameters = '+url_parameters)
 
-    console.log(row+' '+col);
-    response = JSON.parse(httpGet('play?'+url_parameters)); //board='+row+'&col='+col));
-    console.log(response);
-    
-    clickedCellIndex = index =  response['row'] * 3 + response['col'];
-    console.log(clickedCellIndex);
-    clickedCell = document.getElementById(clickedCellIndex);
+        //console.log(row+' '+col);
+        let response = JSON.parse(httpGet('play?'+url_parameters)); //board='+row+'&col='+col));
+        //console.log(response);
+        
+        let clickedCellIndex = response['row'] * 3 + response['col'];
+        //console.log(clickedCellIndex);
+        return clickedCellIndex;
+    }   
+}
+
+function handleBotPlay(index) {
+    console.log('Handle bot play...')
+    let clickedCellIndex = requestBotPlay(index);
+    let clickedCell = document.getElementById(clickedCellIndex);
     console.log(clickedCell);
     handleCellPlayed(clickedCell, clickedCellIndex)
 }
 
 function handleCellClick(clickedCellEvent) {
+    previousBotCell=null;
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
@@ -115,9 +131,11 @@ function handleCellClick(clickedCellEvent) {
     }
 
     handleCellPlayed(clickedCell, clickedCellIndex);
-    handleResultValidation();
-    handleBotPlay(clickedCellIndex);
-    handleResultValidation();
+    let done = handleResultValidation();
+    if (!done) {
+        handleBotPlay(clickedCellIndex);
+        handleResultValidation();
+    }
 }
 
 function handleRestartGame() {
@@ -128,7 +146,36 @@ function handleRestartGame() {
     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
 }
 
+function handleCellIn(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] == '') {
+        // request the bot next move
+        gameState[clickedCellIndex] = currentPlayer;
+        let botClickedCellIndex = requestBotPlay(clickedCellIndex);
+        gameState[clickedCellIndex] = '';
+
+        if(botClickedCellIndex>=0) {
+            let botClickedCell = document.getElementById(botClickedCellIndex);
+            let botPlayer = currentPlayer === "X" ? "O" : "X";
+            botClickedCell.innerHTML = botPlayer;
+            botClickedCell.style.color = 'red';
+            previousBotCell = botClickedCell;
+        }
+    }
+}
+
+function handleCellOut(clickedCellEvent) {
+    if (previousBotCell != null) {
+        previousBotCell.style.color = '';
+        previousBotCell.innerHTML = '';
+        previousBotCell = null;
+    }
+}
 
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('mouseenter', handleCellIn));
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('mouseleave', handleCellOut));
 document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
  
