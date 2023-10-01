a20819eb2b2f76c008e4e28d615df7f79bda5a70
Mário Antunes <mariolpantunes@gmail.com>
2023-10-02 00:37:32 +0100

    Fix bash script

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for EPOCHS in 1000, 3000, 5000 10000 30000 50000 100000; do
+for EPOCHS in 1000 3000 5000 10000 30000 50000 100000; do
     echo -e "Training agent with $EPOCHS."
     python -m src.train -n 30 -e $EPOCHS -o "policies/model_mlp_$EPOCHS.json"
 done
